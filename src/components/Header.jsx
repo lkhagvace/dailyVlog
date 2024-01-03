f6fecd8d950f60582d35baca7f36d5de77459d25
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-01-03 20:05:36 +0800

    1-3 vlog push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,43 +4,8 @@ import { Search } from "./sgvs/Search";
 import Link from "next/link";
 
 export const Header = () => {
-  const array = [
-    "home",
-    "blog",
-    "contact",
-    "facebook",
-    "instagramm",
-    "twitter",
-  ];
-  const newArr = [];
-  let newLetterArr = [];
-  for (let i = 0; i < array.length; i++) {
-    newLetterArr.push(array[i].split(""));
-  }
-  const handleText = (e) => {
-    const value = e.target.value;
-    newArr.pop();
-    newArr.push(value.split(""));
-    if (value === "") {
-      document.getElementById(`container`).style = "display: none";
-      document.getElementById("home").style = "display: none";
-      document.getElementById("facebook").style = "display: none";
-      document.getElementById("instagramm").style = "display: none";
-      document.getElementById("twitter").style = "display: none";
-      document.getElementById("blog").style = "display: none";
-      document.getElementById("contact").style = "display: none";
-    }
-    if (value !== "") {
-      document.getElementById(`container`).style = "display: flex";
-      for (let k = 0; k < array.length; k++) {
-        for (let j = 0; j < newArr.length; j++) {
-          if (newArr[j][0] === newLetterArr[k][j]) {
-            document.getElementById(`${array[k]}`).style = "display: flex";
-          }
-        }
-      }
-    }
-  };
+  const [searchValue, setSearchValue] = useState([]);
+  console.log(searchValue);
   // const [toggle, setToggle] = useState(false);
   // const [isVisible, setIsVisible] = useState(false);
   // isVisible = () => {};
@@ -75,59 +40,17 @@ export const Header = () => {
             <p>Contact</p>
           </a>
         </div>
-        <div className="flex">
-          <div className="relative flex">
-            <input
-              onChange={handleText}
-              id="search"
-              type="text"
-              placeholder="Search"
-            />
-            <div className="absolute right-0 mt-2">
-              <Search />
-            </div>
-          </div>
-          <div id="container" className="flex flex col">
-            <div
-              id="home"
-              className="hidden absolute top-8 bg-black text-white w-32 h-8 rounded-xl justify-center m-auto pt-1"
-            >
-              <Link href={"/"}>Home</Link>
-            </div>
-            <div
-              id="blog"
-              className="hidden absolute top-8 bg-black text-white w-32 h-8 rounded-xl justify-center m-auto pt-1"
-            >
-              <a href="/bloglist">blog</a>
-            </div>
-            <div
-              id="contact"
-              className="hidden absolute top-8 bg-black text-white w-32 h-8 rounded-xl justify-center m-auto pt-1"
-            >
-              <a href="/contact">contact</a>
-            </div>
-            <div
-              id="facebook"
-              className="hidden absolute top-8 bg-black text-white w-32 h-8 rounded-xl justify-center m-auto pt-1"
-            >
-              <a href="https://www.facebook.com/pinecone.academy.mongolia">
-                facebook
-              </a>
-            </div>
-            <div
-              id="instagramm"
-              className="hidden absolute top-8 bg-black text-white w-32 h-8 rounded-xl justify-center m-auto pt-1"
-            >
-              <a href="https://www.instagram.com/pineconemongolia/">
-                instagramm
-              </a>
-            </div>
-            <div
-              id="twitter"
-              className="hidden absolute top-8 bg-black text-white w-32 h-8 rounded-xl justify-center m-auto pt-1"
-            >
-              <a href="https://www.instagram.com/pineconemongolia/">twitter</a>
-            </div>
+        <div className="relative flex">
+          <input
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            id="search"
+            type="text"
+            placeholder="Search"
+          />
+          <div className="absolute right-0 mt-2">
+            <Search />
           </div>
         </div>
       </nav>
